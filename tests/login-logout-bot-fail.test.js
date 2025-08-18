8b44bb27d84a5a61bf831ebd7215689885fbc9d5
MLF-host <55763070+MLF-host@users.noreply.github.com>
2025-08-18 10:56:35 -0500

    Update login-logout-bot-fail.test.js
    Commented out Failed Test

--- tests/login-logout-bot-fail.test.js
+++ tests/login-logout-bot-fail.test.js
@@ -1,23 +1,23 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 // import { USER_NAME, INCORRECT_PASSWORD } from './secrets.js';
 
-test('test', async ({ page }) => {
+// test('test', async ({ page }) => {
     // navigate to Portal login page
-  await page.goto('https://serve.mlf.org/s/');
+  // await page.goto('https://serve.mlf.org/s/');
     // Click Login or Register button
-  await page.getByRole('button', { name: 'Login or Register' }).click();
+  // await page.getByRole('button', { name: 'Login or Register' }).click();
     // Click Username text box
-  await page.getByRole('textbox', { name: 'Username' }).click();
+  // await page.getByRole('textbox', { name: 'Username' }).click();
     // Enter Username
-  await page.getByRole('textbox', { name: 'Username' }).fill(USER_NAME);
+  // await page.getByRole('textbox', { name: 'Username' }).fill(USER_NAME);
     // Click Password text box
-  await page.getByRole('textbox', { name: 'Password' }).click();
+  // await page.getByRole('textbox', { name: 'Password' }).click();
     // Enter incorrect Password
-  await page.getByRole('textbox', { name: 'Password' }).fill(INCORRECT_PASSWORD);
+  // await page.getByRole('textbox', { name: 'Password' }).fill(INCORRECT_PASSWORD);
     // Click Log in button. Test should fail here
-  await page.getByRole('button', { name: 'Log in' }).click();
+  // await page.getByRole('button', { name: 'Log in' }).click();
     // Redirect to Portal login page never happens. Timeout error occurs here
-  await page.getByRole('button', { name: 'User Profile' }).click();
+  // await page.getByRole('button', { name: 'User Profile' }).click();
     // In dropdown menu, click Logout option
-  await page.getByRole('menuitem', { name: 'Logout' }).click();
-});
+  // await page.getByRole('menuitem', { name: 'Logout' }).click();
+// });
